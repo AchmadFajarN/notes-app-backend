575923f08b2addba5cae89f5165ce3dba039bf77
ajay <achmadfajar712@gmail.com>
2025-06-04 13:51:51 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
+// dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
+
+// notes
 const notes = require('./api/notes');
 const NoteService = require('./service/postgres/NoteService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exeptions/ClientError');
 
+// users
+const users = require('./api/users');
+const UserService = require('./service/postgres/UserService');
+const UserValidator = require('./validator/users');
+
 const init = async () => {
   const noteService = new NoteService();
+  const userService = new UserService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,13 +28,22 @@ const init = async () => {
     }
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: noteService,
-      validator: NotesValidator
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: noteService,
+        validator: NotesValidator
+      }
+    },
+    {
+      plugin: users,
+      options: {
+        service: userService,
+        validator: UserValidator
+      }
     }
-  });
+  ]);
   server.ext('onPreResponse', (req, h) => {
     const { response } = req;
 
